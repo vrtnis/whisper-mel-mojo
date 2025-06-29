cf6d518b2a958afe353ce4f10fbdb5457edfe187
vrtnis <123119434+vrtnis@users.noreply.github.com>
2025-06-29 18:21:22 +0000

    declare sigs

--- mojo_mel.py
+++ mojo_mel.py
@@ -4,3 +4,13 @@ import numpy as np
 
 # 1) Load libmel.so
 _lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "libmel.so"))
+
+
+# 2) Declare signature
+_lib.run_pipeline.argtypes = [
+    ctypes.POINTER(ctypes.c_uint8),
+    ctypes.c_int,
+    ctypes.POINTER(ctypes.c_double),
+    ctypes.POINTER(ctypes.c_double),
+]
+_lib.run_pipeline.restype = None
